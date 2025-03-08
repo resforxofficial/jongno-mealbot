988dd47ec8c6b00208ef2bc0436ffcbfeaba8505
resforxofficial <paedeogim@gmail.com>
2025-03-09 02:12:21 +0900

    Update index.py

---
+++ api/index.py
@@ -0,0 +1,11 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+import time
+
+chrome_options = Options()
+chrome_options.add_argument("--disable-gpu")
